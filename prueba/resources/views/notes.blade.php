6f6e327e27fcce11b4735ce83546d154c04ee84f
cristhianpavez <capavez88@gmai.com>
2017-01-10 16:13:55 -0300

    cambio desde source stree

--- prueba/resources/views/notes.blade.php
+++ prueba/resources/views/notes.blade.php
@@ -7,7 +7,7 @@
 		<div class="row">
 			<div class="col-md-6 col-md-offset-1">
 				<div class="panel panel-default">
-					<div class="panel-heading">Notas</div>
+					<div class="panel-heading">Notas Modificadas</div>
 
 					<div class="panel-body">
 						<ul class="list-group">
